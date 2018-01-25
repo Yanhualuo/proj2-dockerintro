30d592e6ac39b8b887f0e3dd748cf6450d7308b0
Yanhua <bfone12@gmail.com>
2018-01-25 11:39:08 -0800

    proj2 first commit, add testing files

--- web/app.py
+++ web/app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!"
+//Error handlers
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def error_403(error):
+    return render_template('403.html'), 403
+
+
+@app.route("/<name>")
+def hello(name):
+    if (len(name) > 1):
+        if ("//" not in name) and ("~" not in name) and (".." not in name):
+            try:
+                return open(name,"r").read()
+            except IOError:
+                  return error_404(404)
+        else:
+            return error_403(403)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
